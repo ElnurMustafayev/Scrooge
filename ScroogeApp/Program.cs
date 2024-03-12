0c3ab507c98cd842b8e264766dfacf73bea3df7c
Elnur Mustafayev <elnur.mustafayeev@gmail.com>
2024-03-12 21:52:19 +0400

    Create Project

---
+++ ScroogeApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+var httpListener = new HttpListener();
+
+httpListener.Prefixes.Add($"http://*:8080/");
+
+httpListener.Start();
+
+System.Console.WriteLine("Server started...");
+
+var client = await httpListener.GetContextAsync();
+
+System.Console.WriteLine(client);
